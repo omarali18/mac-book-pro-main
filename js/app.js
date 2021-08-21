8561fbbcf4f99000c942a6aa7afb68db205578ce
omarali18 <omarali1814000@gmail.com>
2021-08-21 16:57:10 +0600

    total cosnt added

--- js/app.js
+++ js/app.js
@@ -7,12 +7,11 @@ function memoryCard(isadd) {
     else {
         memoryCost.innerText = 180;
     }
-    const extraMemoryCost = memoryCost.innerText;
-    return extraMemoryCost;
+    totalPrice()
 };
 // extra function for storage
 function ssdStorage(isStorageAdd) {
-    const storageCost = document.getElementById("512GBstorage-cost");
+    const storageCost = document.getElementById("storage-cost");
     if (isStorageAdd == true) {
         storageCost.innerText = 0;
     }
@@ -22,8 +21,7 @@ function ssdStorage(isStorageAdd) {
     else {
         storageCost.innerText = 180;
     }
-    const extraStorageCost = storageCost.innerText;
-    return extraStorageCost;
+    totalPrice()
 
 };
 
@@ -35,30 +33,44 @@ function deliveryTime(isCostAdd) {
     else {
         deliveryCost.innerText = 20;
     }
+    totalPrice()
 };
+function extraDevice(device) {
+    const extraDevicePrice = document.getElementById(device + '-cost');
+    const extraDeviceCost = extraDevicePrice.innerText;
+    return parseInt(extraDeviceCost)
+}
 
 function totalPrice() {
-    const bestPrice = document.getElementById()
+    // debugger;
+    const bestPrice = document.getElementById("best-price");
+    const bestPriceNumber = parseInt(bestPrice.innerText);
+    const memoryCard = extraDevice("memory");
+    const storageCard = extraDevice("storage");
+    const deliveryCard = extraDevice("delivery");
+    const netBill = bestPriceNumber + memoryCard + storageCard + deliveryCard;
+    const totalPrice = document.getElementById("total-price");
+    totalPrice.innerText = netBill;
+    const inTotalPrice = document.getElementById("in-total-price");
+    inTotalPrice.innerText = netBill;
 }
 // Extarnal memory event added
 document.getElementById("memory-8GB").addEventListener("click", function () {
-    memoryCard(true)
+    memoryCard(true);
 });
 document.getElementById("memory-16GB").addEventListener("click", function () {
-    memoryCard(false)
+    memoryCard(false);
 });
 
 // Extarnal storage event added
 function storage256GB() {
-    console.log("kire nana 333");
-    ssdStorage(true)
+    ssdStorage(true);
 };
-
 document.getElementById("storage-512GB").addEventListener("click", function () {
-    ssdStorage(false)
+    ssdStorage(false);
 });
 function storage1TB() {
-    ssdStorage("1TB")
+    ssdStorage("1TB");
 };
 
 // Delivery charge event added
